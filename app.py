5180d4cb4cd59118e617fd6389af76841ddb6adf
j.janin-reynaud <julien.janin-reynaud@itsfactory.fr>
2021-02-26 19:02:51 +0100

    clean typo

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World 88!"
+    return "Hello World!"
 
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!!".format(name)
+    return "Hello {}!".format(name)
 
 
 if __name__ == '__main__':
